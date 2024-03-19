fc1c8d74330275d0d18bf7cf823d6a716017da74
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-03-19 16:36:16 +0000

    config: eslint config

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  NODE_ENV: z.enum(["dev", "test", "prod"]).default("dev"),
-  DATABASE_URL: z.string().default(""),
-  JWT_SECRET: z.string().default(""),
-});
+  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
+  DATABASE_URL: z.string().default(''),
+  JWT_SECRET: z.string().default(''),
+})
 
-export const _env = envSchema.safeParse(process.env);
+export const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  console.error("Environment validation error:", _env.error.message);
+  console.error('Environment validation error:', _env.error.message)
 
-  throw new Error("Environment validation error");
+  throw new Error('Environment validation error')
 }
 
-export const env = _env.data;
+export const env = _env.data
