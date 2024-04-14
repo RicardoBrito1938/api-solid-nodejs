108badde35d464c94a04bc30bdeac1f6282e9ea6
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-04-14 10:58:55 +0100

    feature: metrics

---
+++ src/use-cases/get-user-check-in.spec.ts
@@ -0,0 +1,26 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { GetUserMetricsCase } from './get-user-metrics'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: GetUserMetricsCase
+
+describe('Fet User Metrics Use Case', () => {
+  beforeEach(async () => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserMetricsCase(checkInsRepository)
+  })
+
+  it('should be able to get user metrics, number of checkins', async () => {
+    for (let i = 1; i <= 2; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkInsCount } = await sut.execute({ userId: 'user-01' })
+
+    expect(checkInsCount).toEqual(2)
+  })
+})
