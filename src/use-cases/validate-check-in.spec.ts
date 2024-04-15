0aab4feca1a941aa4048ace420f4d616aace113e
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-04-15 07:56:19 +0100

    feature: validate check in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 
@@ -9,6 +9,12 @@ describe('Validate check in', () => {
   beforeEach(() => {
     inMemoryCheckInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(inMemoryCheckInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be to validate check in', async () => {
@@ -20,5 +26,24 @@ describe('Validate check in', () => {
     const response = await sut.execute({ checkInId: checkIn.id })
 
     expect(response.checkIn.validated_at).not.toBe(null)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await inMemoryCheckInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
