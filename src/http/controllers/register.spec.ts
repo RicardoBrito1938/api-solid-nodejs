53e6721aecab08f6191aca6289c1c9d3b25cb978
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-04-30 16:02:29 +0100

    feature: e2e tests for register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Register e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+    })
+
+    expect(response.status).toBe(201)
+  })
+})
