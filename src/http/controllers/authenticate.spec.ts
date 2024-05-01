2e6907f4016a5d72bd4b3df0ec065e139a8c923b
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-05-01 16:56:24 +0100

    test: e2e authenticate

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,29 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Authenticate e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should register a user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'password',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: 'password',
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.body).toHaveProperty('token')
+  })
+})
